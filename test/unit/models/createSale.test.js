0afabde56e94abc1787258d6c42c475a31622f7e
Guilherme <guilhermehallmann@gmail.com>
2022-05-17 08:40:27 -0300

    feat: testing create on sales model layer

--- test/unit/models/createSale.test.js
+++ test/unit/models/createSale.test.js
@@ -2,3 +2,23 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const salesModel = require('../../../models/salesModel');
 const connection = require('../../../models/connection');
+
+describe('Creates a new saleId on the DB', () => {
+
+  before(() => {
+    const resultExec = [{ insertId: 1 }];
+    sinon.stub(connection, 'execute').resolves(resultExec);
+  })
+
+  after(() => {
+    connection.execute.restore();
+  })
+  
+  describe('When its successfully created', async () => {
+    it('Should return an id number', async () => {
+      const result = await salesModel.create();
+      console.log(result)
+      expect(result).to.be.equal(1);
+    })
+  })
+})
